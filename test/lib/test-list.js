c67ee5998fe9f32cb1d46bc7078723d316cf16d7
khai96_ <hvksmr1996@gmail.com>
2017-04-12 15:47:06 +0700

    Improve test-list

--- test/lib/test-list.js
+++ test/lib/test-list.js
@@ -1,17 +1,19 @@
 'use strict'
 
 const {stdout, stderr} = require('process')
+const testmsg = (stream, description, state) => stream.write(`  :: ${description} -> ${state}\n`)
 
 module.exports = (...units) => units
   .map(
     ([description, action]) => {
-      stdout.write(description + '... ')
       try {
         const passed = action()
-        passed ? stdout.write('passed\n') : stderr.write('failed\n')
+        passed
+          ? testmsg(stdout, description, 'passed')
+          : testmsg(stderr, description, 'failed')
         return passed
       } catch (error) {
-        stderr.write(`errored\nerror`)
+        testmsg(stderr, description, `errored\n${error}`)
         return false
       }
     }
