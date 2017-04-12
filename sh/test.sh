5f8bf4060e751569d0ba141782cce3c4553944b8
khai96_ <hvksmr1996@gmail.com>
2017-04-12 15:47:27 +0700

    Add unit-test

--- sh/test.sh
+++ sh/test.sh
@@ -8,4 +8,14 @@
     cat stdout.tmp
     exit 2
   )
+) && (
+  printf "Unit Test... "
+  node > stdout.tmp 2> stderr.tmp && (
+    echo "passed"
+  ) || (
+    exitcode=$?
+    echo "failed" >&2
+    cat stderr.tmp >&2
+    cat stdout.tmp
+  )
 )
